6571f0172705ee4e4d6560d31a5236b408c5ed21
ironjemo <megamovi@hotmail.es>
2025-03-13 18:22:44 -0500

    Add new code for age calculation

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,12 +1,41 @@
-﻿namespace HelloWorld{
-    class Program {
-       
-        static void Main(){
-             var Name= "Jhon";
-             Name= "Jhon 1";
-            Console.WriteLine($"Hello world.:{Name}");
-            Console.WriteLine();
+﻿namespace HelloWorld
+{//Creacion de clase
+    class Program
+    {
+
+        static void Main()
+        {//metodo statico se puede acceder sin nesecidad de una instancia
+            DateOnly dateConverted = new DateOnly();
+            String nameInput;
+            String birthdayInput;
+            Console.WriteLine("Hola bienvenido al calculador de años");
+            Console.WriteLine("Escribe tu nombre: ");
+            nameInput = Console.ReadLine();
+            Console.WriteLine($"Un gusto conocerte (nameImput)");
+            Console.WriteLine($"Escribe tu fecha de nacimiento dd/mm/yy:");
+             birthdayInput = Console.ReadLine();
+            bool isDateValid = DateOnly.TryParse(birthdayInput,out dateConverted);
+            if(isDateValid==false)   Console.WriteLine($"La fecha de nacimiento es invalida, usted nos envio este dato erroneo{birthdayInput}");
+            var Person = new Person{
+                Name=nameInput,
+                Birthday=dateConverted,
+                Age= DateTime.Now.Year-dateConverted.Year
+            };
+            Console.WriteLine($"Tu nombre: {Person.Name}");
+            Console.WriteLine($"Tu fecha de nacimiento: {Person.Birthday}");
+            Console.WriteLine($"Tu edad es : {Person.Age} años!");
+            Console.WriteLine();//se genera un tiempo activandolo por una tecla
         }
 
     }
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public DateOnly Birthday { get; set; }
+    }
+
 }
