cb6c7726adeb1fbcbaff4e02f0a418ea8a9b90e8
ironjemo <megamovi@hotmail.es>
2025-03-14 16:48:56 -0500

    New changes and incorporate some Operators

---
+++ conditionals_and_operators/Program.cs
@@ -0,0 +1,23 @@
+﻿// /*Conditionals*/
+//  Console.WriteLine("Ingrese un numero: ");
+//  int num = int.Parse(Console.ReadLine());
+//  if (num > 0) Console.WriteLine("Positivo");
+//  else if (num < 0) Console.WriteLine("Negativo");
+//  else  Console.WriteLine("Es cero");
+
+/*End Conditionals*/
+
+
+/*Operators*/
+Console.WriteLine("Ingrese el primer numero: ");//Se ingresa el primer dato
+double a= double.Parse(Console.ReadLine());//Se convierte el dato ingresado a un tipo double
+Console.WriteLine("Ingrese el segundo numero");
+double b = double.Parse(Console.ReadLine());
+const string Indeterminado = "Indeterminado";
+Console.WriteLine($"Suma: {a+b}, Resta: {a-b}, Multiplicacion: {a*b}, Division: {(b!=0? a/b: Indeterminado)}");
+
+
+/*End Conditionals*/
+
+
+
